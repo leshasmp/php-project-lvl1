bdc8220f222a0b892a70834890b996d0885f70df
alexey-melekhov1989 <alexey-melekhov1989@yandex.ru>
2020-11-17 00:33:24 +0500

    first commit

---
+++ src/Cli.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+
+class Ask
+{
+    public static function askUserName()
+    {
+        line('Welcome to the Brain Game!');
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
+    }
+}
+
